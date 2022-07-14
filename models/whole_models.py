f009838d499264a1badd4133ea99f17eac0da7f7
coffeetumbler <gs12108@gmail.com>
2022-07-14 21:39:24 +0900

    add complete model

---
+++ models/whole_models.py
@@ -0,0 +1,86 @@
+import os, sys
+sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
+
+import numpy as np
+
+import torch
+import torch.nn as nn
+
+from models.transformer import get_transformer_encoder, get_transformer_decoder
+from models.submodels import EmbeddingLayer, ReconstructionBlock
+
+
+
+# Whole SR Transformer model
+class SRTransformer(nn.Module):
+    """
+    Args:
+        lr_img_res : size of low-resolution image
+        upscale : upscaling factor, 2 to 4
+        patch_size : size of image patch
+        window_size : size of window
+        d_embed : embedding dimension
+        encoder_n_layer : number of layers in encoder
+        decoder_n_layer : number of layers in decoder
+        n_head : number of heads in self-attention module
+        dropout : dropout ratio
+    """
+    def __init__(self, lr_img_res=48, upscale=2, patch_size=2, window_size=4, d_embed=128,
+                 encoder_n_layer=12, decoder_n_layer=12, n_head=4, dropout=0.1):
+        super(SRTransformer, self).__init__()
+        assert lr_img_res % (patch_size * window_size) == 0
+        assert d_embed % n_head == 0
+        
+        # numbers of patches along an axis
+        encoder_n_patch = lr_img_res // patch_size
+        decoder_n_patch = encoder_n_patch * upscale
+        
+        # embedding layers and reconstruction block
+        self.encoder_embedding_layer = EmbeddingLayer(patch_size, d_embed)
+        self.decoder_embedding_layer = EmbeddingLayer(patch_size, d_embed)
+        self.reconstruction_block = ReconstructionBlock(patch_size, d_embed, d_embed*4, dropout)
+        
+        # encoder
+        self.transformer_encoder = get_transformer_encoder(d_embed=d_embed,
+                                                           positional_encoding=None,
+                                                           relative_position_embedding=True,
+                                                           n_layer=encoder_n_layer,
+                                                           n_head=n_head,
+                                                           d_ff=d_embed*4,
+                                                           n_patch=encoder_n_patch,
+                                                           window_size=window_size,
+                                                           dropout=dropout)
+
+        # decoder
+        if upscale > 1 and upscale < 4:
+            self.transformer_decoder = get_transformer_decoder(d_embed=d_embed,
+                                                               positional_encoding=None,
+                                                               relative_position_embedding=True,
+                                                               n_layer=decoder_n_layer,
+                                                               n_head=n_head,
+                                                               d_ff=d_embed*4,
+                                                               query_n_patch=decoder_n_patch,
+                                                               query_window_size=window_size*upscale,
+                                                               key_n_patch=encoder_n_patch,
+                                                               key_window_size=window_size,
+                                                               dropout=dropout)
+        elif upscale == 4:
+            """Need to be filled."""
+        
+    def forward(self, lr_img, lr_img_upscaled):
+        """
+        <input>
+            lr_img : (n_batch, 3, img_height, img_width), low-res image
+            lr_img_upscaled : (n_batch, 3, upscale*img_height, upscale*img_width), upscaled lr_img by bicubic interpolation
+            
+        <output>
+            hr_img : (n_batch, 3, upscale*img_height, upscale*img_width), high-res image
+        """
+        # Encode low-resolution image
+        lr_img = self.encoder_embedding_layer(lr_img)
+        lr_img = self.transformer_encoder(lr_img)
+        
+        # Make high-resolution image
+        out = self.decoder_embedding_layer(lr_img_upscaled)
+        out = self.transformer_decoder(out, lr_img)
+        return lr_img_upscaled + self.reconstruction_block(out)
